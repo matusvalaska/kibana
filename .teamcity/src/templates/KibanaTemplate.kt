4732bcc6314c6847dfa2fd7b77bba4da34cd54cd
Brian Seeders <brian.seeders@elastic.co>
2020-10-16 11:46:07 -0400

    Some cleanup and add profile id req in another spot

--- .teamcity/src/templates/KibanaTemplate.kt
+++ .teamcity/src/templates/KibanaTemplate.kt
@@ -1,22 +1,3 @@
-/*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package templates
 
 import vcs.Kibana
@@ -39,7 +20,7 @@ object KibanaTemplate : Template({
   }
 
   requirements {
-    // equals("system.cloud.profile_id", "kibana")
+    equals("system.cloud.profile_id", "kibana", "RQ_CLOUD_PROFILE_ID")
     startsWith("teamcity.agent.name", "kibana-standard-2-", "RQ_AGENT_NAME")
   }
 
